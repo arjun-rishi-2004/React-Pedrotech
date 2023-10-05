bdcd801798aab18b0bf91546ff64b890fcde13ee
arjun-rishi-2004 <arjunrishi478@gmail.com>
2023-10-05 22:16:18 +0530

    Props and Components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World
+<Job company="Google" position="Senior SDE" salary={200000} />
+<Job company="Facebook" position="Junior SDE" salary={90000} />
+<Job company="Microsoft" position="Data Scientist" salary={600000} />
+
+  
+  
     </div>
   );
+}
+
+function Job(props){
+return (
+  <div>
+    <h1>{props.company}</h1>
+    <h2>{props.position}</h2>
+    <h1>{props.salary}</h1>
+
+  </div>
+)
 }
 
 export default App;
