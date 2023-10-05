b5dd893f8d1f22101248c2d6e26abb8517a80a36
arjun-rishi-2004 <arjunrishi478@gmail.com>
2023-10-05 23:07:48 +0530

    Style,conditional rendering and list

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import './App.css';
 
 function App() {
+  const planets=[
+{name:"Earth" ,isGasPlanet:false},
+{name:"Mars" ,isGasPlanet:true},
+{name:"Jupiter" ,isGasPlanet:true},
+{name:"Mercury" ,isGasPlanet:false},
+{name:"Pluto" ,isGasPlanet:false},
+  ]
+
   return (
     <div className="App">
-<Job company="Google" position="Senior SDE" salary={200000} />
-<Job company="Facebook" position="Junior SDE" salary={90000} />
-<Job company="Microsoft" position="Data Scientist" salary={600000} />
-
-  
-  
-    </div>
-  );
+      
+          {/* // <Planet name="Earth" isGasPlanet={true} />
+          // <Planet props={planet}/> */}
+        <DisplayAllPlanets props={planets}/>
+      
+</div>
+  )
+}
+function DisplayAllPlanets({ props }) {
+  return props.map((prop) => (
+    <Planet name={prop.name} isGasPlanet={prop.isGasPlanet} />
+  ));
 }
 
-function Job(props){
-return (
-  <div>
-    <h1>{props.company}</h1>
-    <h2>{props.position}</h2>
-    <h1>{props.salary}</h1>
 
-  </div>
-)
+function Planet(props){
+  console.log(props)
+  return (
+    props.isGasPlanet &&
+    <div>
+      <h1 style={{color:props.name=="Mars" && "red"}}>{props.name}</h1>
+    </div>
+  )
+
 }
 
 export default App;
