9692377ca5355e33dcef9d7c07c6bfc5c53f9c8a
arjun-rishi-2004 <arjunrishi478@gmail.com>
2023-10-08 13:02:31 +0530

    useState with function ,arrow functions,anony func

--- src/App.js
+++ src/App.js
@@ -1,48 +1,24 @@
 import './App.css';
+import {useState} from 'react'
 
 function App() {
-  const planets=[
-{name:"Earth" ,isGasPlanet:false},
-{name:"Mars" ,isGasPlanet:true},
-{name:"Jupiter" ,isGasPlanet:true},
-{name:"Mercury" ,isGasPlanet:false},
-{name:"Pluto" ,isGasPlanet:false},
-  ]
-
-  return (
-    <div className="App">
-      
-          {/* // <Planet name="Earth" isGasPlanet={true} />
-          // <Planet props={planet}/> */}
-        {/* <DisplayAllPlanets props={planets}/> */}
-      <Child name="Arjun" dept="CSE"/>
-      <Student name="Deva" dept="CSBS"/>
-</div>
-  )
-}
-function DisplayAllPlanets({ props }) {
-  console.log("Prps",props)
-  return props.map((prop) => (
-    <Planet name={prop.name} isGasPlanet={prop.isGasPlanet} />
-  ));
+  
+const [count,setCount]=useState(0);
+function decrease(){
+  setCount(count-1);
 }
 
-function Child({name,dept}){
-console.log(name,dept);
+const setToZero = ()=>{
+  setCount(0);
 }
-
-function Student({name}){
-  console.log(name);
-
-}
-
-
-function Planet(props){
-  console.log(props);
-  return (
-    props.isGasPlanet &&
+  
+return (
     <div>
-      <h1 style={{color:props.name=="Mars" && "red"}}>{props.name}</h1>
+      <center> <h1>{count} </h1>
+       <button onClick={()=>{setCount(count+1)}}>Increase</button>
+       <button onClick={decrease} >Decrease</button>
+       <button onClick={setToZero}>Set To zero</button>
+       </center>
     </div>
   )
 
